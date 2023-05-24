19308c07be5487befaffe15340c631894a857182
Safiullova <e.safiullova2000@mail.ru>
2023-05-25 00:58:46 +0400

    add functions

--- script.js
+++ script.js
@@ -1,160 +1,111 @@
-let myForms = document.forms.myForm;    //нашли форму
-let elemCarBrend = myForms.elements.carBrend.value;     //нашла элемент бренда автомобиля,без значения
-let elemCarModel = myForms.elements.carModel.value;   // элемент марки автомобиля
-let elemPetrol = myForms.elements.fuel.value;   //элемент инпута со значением "Бензин"
-let elemDiesel = myForms.elements.fuel.value;  //элемент инпута со значением "Дизель"
-let elemGas = myForms.elements.fuel.value;        //элемент инпута со значением "Газ"
-let elemElectric = myForms.elements.fuel.value;    //элемент инпута со значением "Электричество"
-let elemEnСapacity = myForms.elements.enСapacity.value;    // элемент обьема двигателя
-let elemNewCar = myForms.elements.newCar.checked;   // элемент нового автомобиля
-let elemSuppСar = myForms.elements.suppСar;  //элемент поддержанного автомобиля
-let elemBankСard = myForms.elements.bankСard.checked; // оплата картой
-let elemСash = myForms.elements.cash.checked; // оплата картой
-
-let button = document.querySelector(".button"); // нашла кнопку "расчитать стоимость"
+// нахожу элементы, записываю в переменные
+let elemOldСar = document.forms.car.elements.oldСar;  //элемент формы подержанного автомобиля
+let button = document.querySelector(".button"); // переменная для кнопки
 let carOwner = document.querySelector(".carOwner"); // нашла див с количеством владельцев
-let totalAm = document.querySelector(".sum");
 
 
+function onChangeFirst() {  // эта функция подгружает массив выбора моделей для разных марок
+  const nameValue = document.getElementById('carName').value;
+  
+  const reno = [{ model:'re', price: 100000 } , {model: 'ree', price: 150000 },{model:'reee', price: 200000 }];
+  const opel = [{model:'op', price: 150000 }, {model:'opp', price: 170000 },{model:'oppp', price: 220000 }];
+  const mazda = [{model:'ma', price: 110000 }, {model:'maa', price: 120000 },{model:'maaa', price: 130000 }];
+  const jaguar = [{model:'ja', price: 140000 }, {model:'jaa', price: 160000 },{model:'jaaa', price: 180000 }];
+
+  let values=[] // переменная для массива со списком моделей
+
+  document.getElementById("carModel").innerHTML=''; // Для очистки списка моделей, при изменении выбора марки машины
+  if(nameValue == 1){
+  values = [...reno];    
+  }
+  else if(nameValue == 2){
+    values = [...opel]
+  }
+  else if(nameValue == 3){
+    values = [...mazda]
+  }
+  else if(nameValue == 4){
+      values = [...jaguar]
+    }
+
+  let select = document.getElementById('carModel'); // завожу переменную для выпадающего списка моделей
+
+  for (const val of values)
+  {
+      let option = document.createElement("option"); // добавляю опции для выпадающего списка моделей
+      option.value = val.price;
+      option.text = val.model;
+      select.appendChild(option); // добавляю список моделей
+  }};
 
 
-myForms.addEventListener("submit", function(event) { //основная функция
-   event.preventDefault();
+function addcarOwner(){ // если авто подержанное, то появляются родиокнопки с количеством владельцев
 
+if(elemOldСar.value !=="") 
+{
+  const oneCarOwner = `<div><p>1-2 владельца</p><input type="radio" name="owner" value="500"></div>`;
+  const threeCarOwner = `<div><p>3 и более владельцев</p><input type="radio" name="owner" value="100"></div>`;
+  carOwner.insertAdjacentHTML('beforeEnd', oneCarOwner);
+  carOwner.insertAdjacentHTML('beforeEnd', threeCarOwner);
+}}
+elemOldСar.addEventListener('input', function (evt) {
+  evt.preventDefault();
+  addcarOwner();
+});
 
+button.addEventListener("click", function(event) { //основная функция для расчета
+  event.preventDefault();
 
-   let brend =document.querySelector("#carBrend"); 
    let model =document.querySelector("#carModel").value;
-   let petrol =document.querySelector("#petrol");
-   let diesel =document.querySelector("#diesel");
-   let gas =document.querySelector("#gas");
-   let electric =document.querySelector("#electric");
-   let enСapacity =document.querySelector("#enСapacity").value;
    let newCar =document.querySelector("#newCar");
-   let suppСar =document.querySelector("#suppСar");
-
-   let petrolVal = petrol.value;
-   let dieselVal = diesel.value;
-   let gasVal =gas.value;
-   let electricVal = electric.value;
+   let oldСar =document.querySelector("#oldСar");
 
    let newCarVal = newCar.value;
-   let suppСarVal = suppСar.value;
+   let oldСarVal = oldСar.value;
 
+   let result =0; // переменная для расчета стоимости авто
 
-
-   let sum =0;
-
- sum =  sum + +model;
-    console.log(sum);
+   result =  result + +model; // стоимость, если выбрана только модель.
+    console.log(result);
     
-    if (petrol.checked && petrolVal) {
-      sum = sum + +petrolVal;
-      console.log(sum);
+      
+    const fuels = document.querySelectorAll('input[name="fuel"]'); // смотрим какое топливо выбрано
+        let fuelCheck;
+        for (const fuel of fuels) {
+            if (fuel.checked) {
+              fuelCheck = fuel.value;
+                break;
+            }
+        }
+        result = result + +fuelCheck; // стоимость с учетом выбора топлива
+        console.log(result);
+
+const capacity = document.forms.capa.elements.capacity; // записываю инпут объема двигателя в переменную
+result = result + +capacity.value; // Зависимость цены от объема двигателя. я не знаю какая реальная математическая зависимость))
+console.log(result);
+
+
+    if (newCar.checked) {  // стоимость с наценкой на новый авто.
+      result = result + +newCarVal;
+      console.log(result);
     }
     
-     if (diesel.checked && dieselVal) {
-      sum = sum + +dieselVal;
-      console.log(sum);
-    } 
-     if (gas.checked && gasVal) {
-      sum = sum + +gasVal;
-      console.log(sum);
-    } 
-    if(electric.checked && electricVal) {
-      sum = sum + +electricVal;
-      console.log(sum);
-    }
-  
-    // console.log(sum = ${sum});
+    else if (oldСar.checked) {// стоимость с наценкой на подержаный авто.
 
- 
-    if (newCar.checked && newCarVal) {
-      sum = sum + +newCarVal;
-      console.log(sum);
-    }
-    
-     if (suppСar.checked && suppСarVal) {
-      sum = sum + +suppСarVal;
-      console.log(sum);
-    } 
+      result = result + +oldСarVal;
+      console.log(result);
+    };
 
-     totalAm.textContent = sum;
+    const cash = document.querySelector('#cash'); // скидка за оплату наличкой
+    if (cash.checked) {
+      result = result - +result*0.1;
+    };
+    console.log(result);
+
+let resultSum = document.querySelector('.result'); // вывод результата 
+resultSum.insertAdjacentText('beforeend', result); // итоговая стоимость
 
 
 });
 
-
-
-
-function onChangeFirst() {  //функция подгружает дополнительный массив
-    const value = document.getElementById('carBrend').value;
-    
-    const jaguar = [{ model:'FX', price: 100000 } , {model: 'XJ', price: 150000 },{model:'XE', price: 300000 }];
-    const hyundai = [{model:'Creta', price: 120000 }, {model:'Solaris', price: 170000 },{model:'Tucson', price: 320000 }];
-    const reno = [{model:'Kaptur', price: 110000 }, {model:'Koleos', price: 160000 },{model:'Symbol', price: 310000 }];
-    const mazda = [{model:'CX-3', price: 110000 }, {model:'CX-5', price: 160000 },{model:'MX-30', price: 310000 }];
-
-    let values=[]
-
-    document.getElementById("carModel").innerHTML=''
-    if(value==0){
-        document.getElementById('carModel').value=''
-        document.getElementById("carModel").disabled = true;
-        return
-      }
-      else if(value == 1){
-
-    values = [...jaguar];    
-    }
-    else if(value == 2){
-      values = [...hyundai]
-    }
-    else if(value == 3){
-      values = [...reno]
-    }
-    else if(value == 4){
-        values = [...mazda]
-      }
-     
-    let select = document.getElementById('carModel');
-
-    for (const val of values)
-    {
-        let option = document.createElement("option");
-        option.value = val.price;
-        option.text = val.model;
-
-        select.appendChild(option);
-    }
-    document.getElementById('carModel').disabled=false
-}
-
-
-function addcarOwner(){ //проверяем условие если авто поддержанное то появляются родиокнопки с количеством владельцев
-
-if(elemSuppСar.value !=="") 
-{
-    const newCarOwner = `<div><p>1-2 владельца</p><input  type="radio"></div>`;
-    const toCarOwner = `<div><p>3 и более владельцев</p><input type="radio"></div>`;
-    carOwner.insertAdjacentHTML('beforeEnd', newCarOwner);
-    carOwner.insertAdjacentHTML('beforeEnd', toCarOwner);
-}
-}
-elemSuppСar.addEventListener('input', function (evt) {
-    evt.preventDefault();
-    addcarOwner();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // form.reset(); // сбрасываем все поля после обработки формы
